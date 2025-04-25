826b8dc90196025f3bdd0528b2d85af0e1aca2d8
amanda baker <amandab@amandas-MacBook-Pro.local>
2025-04-25 15:36:37 -0400

    Initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { NextAuthProvider } from "./Providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -19,10 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <NextAuthProvider>
+          <div className="max-w-3xl mx-auto">
+            <Navbar />
+            {children}
+          </div>
+        </NextAuthProvider>
       </body>
     </html>
   );
